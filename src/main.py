994e8b6a36bb04958ca778c5098368a4588d6e13
SuslikSenya <alexslkarate@gmail.com>
2024-10-31 21:58:41 +0200

    Starting with main file

--- src/main.py
+++ src/main.py
@@ -2,17 +2,19 @@ from fastapi import FastAPI, Request
 import uvicorn
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
-from routers.healthcheck import router as router_healthcheck
 
-app = FastAPI()
+from src.endpoints import user_router, record_router, category_router
+
+app = FastAPI(debug=True)
 
 templates = Jinja2Templates(directory="templates")
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
-app.include_router(router_healthcheck)
-
+app.include_router(user_router)
+app.include_router(category_router)
+app.include_router(record_router)
 
 if __name__ == '__main__':
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
