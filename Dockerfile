f82b882e1be717ddefdf710210db42d0e7797b70
SuslikSenya <alexslkarate@gmail.com>
2024-12-09 18:42:28 +0200

    fixed docker files

--- Dockerfile
+++ Dockerfile
@@ -12,4 +12,6 @@ WORKDIR /src
 
 EXPOSE 8000
 
+ENV DATABASE_URL="postgresql+asyncpg://sasha:NOV87CMvgEy7OKXP7kkOn0tgAUENFAeL@dpg-ctad51jtq21c73c3jvpg-a.oregon-postgres.render.com/lab3_au3f"
+
 CMD ["gunicorn", "-w", "4", "-k", "uvicorn.workers.UvicornWorker", "main:app", "-b", "0.0.0.0:8000"]
--- src/database.py
+++ src/database.py
@@ -3,17 +3,17 @@ from typing import AsyncGenerator
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 from sqlalchemy.ext.declarative import DeclarativeMeta, declarative_base
 
-DATABASE_URL = f"sqlite+aiosqlite:///src/test.db"
-
-Base: DeclarativeMeta = declarative_base()
-
-engine = create_async_engine(DATABASE_URL)
-
-# from config import DB_CONNECTION
+# DATABASE_URL = f"sqlite+aiosqlite:///src/test.db"
 #
 # Base: DeclarativeMeta = declarative_base()
 #
-# engine = create_async_engine(DB_CONNECTION)
+# engine = create_async_engine(DATABASE_URL)
+
+from config import DB_CONNECTION
+
+Base: DeclarativeMeta = declarative_base()
+
+engine = create_async_engine(DB_CONNECTION)
 
 async_session_maker = async_sessionmaker(engine, expire_on_commit=False)
 
