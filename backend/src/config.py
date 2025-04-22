6238e78b99b34822d3d8cfe1410e73a7aec31756
Senya <106750550+SuslikSenya@users.noreply.github.com>
2025-04-22 13:41:16 +0300

    Update config.py

--- backend/src/config.py
+++ backend/src/config.py
@@ -11,5 +11,4 @@ DB_HOST = os.environ.get("DB_HOST")
 DB_PORT = os.environ.get("DB_PORT")
 DB_NAME = os.environ.get("DB_NAME")
 
-# DB_CONNECTION = 'postgresql+asyncpg://sasha:Tsa0zS5iXO1Y6xyfvJXtDCVAcUuZAqQ6@dpg-cvdvtpnnoe9s73ejduvg-a.oregon-postgres.render.com/test_xaa1'
 DB_CONNECTION = os.environ.get("DB_CONNECTION")
